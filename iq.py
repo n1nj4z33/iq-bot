596c23900eb8bfea2a9ac9084a3a17b48be28901
ninjazee <ninja_zee@sibmail.com>
2014-06-04 23:40:29 +0400

    Update iq.py

--- iq.py
+++ iq.py
@@ -25,7 +25,7 @@ SELL_TEXT = 'Sell'
 TURBO = u'Турбо опцион'
 BIN = u'Бинарный опцион'
 TEST_MESSAGES = ['Buy', 'Sell']
-MODES = ['TEST', 'REAL', 'DEMO']
+MODES = ['test', 'real', 'demo']
 
 #Locators
 LOGIN_BUTTON = '//button[@ng-click="login()"]'
@@ -202,7 +202,7 @@ class Iq():
         print '-' * 19
         while True:
             begin_balance = self.get_balance()
-            if mode == 'TEST':
+            if mode == 'test':
                 updated_message = random.choice(TEST_MESSAGES)
             else:
                 work_message = self.get_message_text()
@@ -220,7 +220,7 @@ if __name__ == '__main__':
     PARSER = OptionParser(usage='''Usage: iq.py
         -u <'''u'''Email пользователя>
         -p <'''u'''Пароль пользователя>
-        -m <'''u'''Выбор режима работы [DEMO,REAL,TEST]>
+        -m <'''u'''Выбор режима работы [demo,real,test]>
         -o <'''u'''Выбор опциона [turbo,bin]>
         -l <'''u'''Выбор языка MT alert окна [eng,rus]>
         -a <'''u'''Выбор актива [EUR/USD, BITCOIN]>''',
@@ -236,7 +236,7 @@ if __name__ == '__main__':
     PARSER.add_option('-m', '--mode',
         dest='mode',
         default='',
-        help=u'Выбор режима работы [DEMO,REAL,TEST]',)
+        help=u'Выбор режима работы [demo,real,test]',)
     PARSER.add_option('-o', '--option',
         dest='option',
         default='bin',
