6dc8f8130109350837bb52a0da4f3ec24fbfc0cd
Ivan Skiridomov <Ivan.Skiridomov@gmail.com>
2016-05-02 15:39:58 +0300

    Succes buy

--- bot.py
+++ bot.py
@@ -2,40 +2,102 @@
 # -*- coding: utf-8 -*-
 
 import json
+
+import datetime
+import logging
+
+logger = logging.getLogger()
+FORMAT = "[%(filename)s:%(lineno)s - %(funcName)20s() ] %(message)s"
+logging.basicConfig(format=FORMAT)
+logger.setLevel(logging.DEBUG)
+
+
 import websocket
 import time
 import requests
+import enum
+
+from enum import Enum
+
+class Direction(Enum):
+    call = 'call'
+    put = 'put'
 
 skey = None
 
-def on_message(ws, message):
-    delta = 0
-    localtime = int(round(time.time() * 1000))
-    global skey
+buyTime = 0
+buyPrice = 0
+buyed = False
+exp_time_seconds = 0
 
+def buyActive(ws, direction, buyTime, expTimeInSeconds):
+    logging.info("Посылаем запрос на покупку ...")
+    ws.send(json.dumps({"name": "buy",
+                        "msg": {
+                            "price": 10,
+                            "exp": expTimeInSeconds,
+                            "act": 1,
+                            "type": "turbo",
+                            "time": buyTime,
+                            "direction": direction
+                        }}))
+
+def on_message(ws, message):
+    #print message
+    global skey
+    global buyPrice
+    global buyTime
+    global buyed
+    global exp_time_seconds
     raw = json.loads(message)
-    if (raw['name'] == 'timeSync'):
+    if 'timeSync' in raw['name']:
         servertime = int(raw['msg'])
 
+        # Get EXP time from server time + 1 minute
+        server_time = datetime.datetime.fromtimestamp(servertime/1000)
+        exp_time = server_time + datetime.timedelta(minutes=1)
 
-        delta = localtime - servertime
+        if server_time.second > 30:
+            exp_time = server_time + datetime.timedelta(minutes=2)
 
-        #print time_str
-    if (raw['name'] == 'profile'):
+        exp_time = exp_time.replace(second=0, microsecond=0)
+        exp_time_seconds = int(time.mktime(exp_time.timetuple()))
 
-        skey = raw['msg']['skey']
-        print("Skey: {}".format(skey))
+        #print "Exp time: {} - {}".format(exp_time , exp_time_seconds)
 
-    if (raw['name'] == 'tradersPulse'):
+        buyTime = int(servertime/1000)
+
+    elif 'profile' in raw['name']:
+        if 'skey' in raw['msg']:
+            skey = raw['msg']['skey']
+            logging.debug("Skey: {}".format(skey))
+
+    elif 'newChartData' in raw['name']:
+
+        buyPrice = raw['msg']['value']
+        if not buyed:
+            buyActive(ws,Direction.call,buyTime,exp_time_seconds)
+            buyed = True
+
+    elif 'buyComplete' in raw['name']:
+        logging.debug(message)
+        success = raw['msg']['isSuccessful']
+        if success:
+            logging.info("Куплено ...")
+
+        else:
+            logging.info("Не смог купить актив: {}".format(raw['msg']))
+
+    elif 'listInfoData' in raw['name']:
+        profit = raw['msg'][0]['profit']
+        if profit > 0:
+            logging.info("Выиграли.")
+        else:
+            logging.info("Проиграли увеличиваем ставку")
+
+    elif raw['name'] == 'tradersPulse':
         value = raw['msg']
 
-        bulls = True if value['76'] > 60 else False
-        print skey
-        if not bulls:
-            ws.send(json.dumps({"name":"buy","msg":{"price":10,"refund_value":0,"act":76,"exp":1462026060,"type":"turbo","direction":"put","value":1.142916,"time":1462026030,"skey":skey}}))
-        else:
-            ws.send(json.dumps({"name":"buy","msg":{"price":10,"refund_value":0,"act":76,"exp":1462026060,"type":"turbo","direction":"call","value":1.142916,"time":1462026030,"skey":skey}}))
-        print bulls
     else:
         print message
 
@@ -45,31 +107,28 @@ def on_error(ws, error):
 
 
 def on_close(ws):
-    print "### closed ###"
+    logging.info("### closed ###")
 
 
 def on_open(ws):
-    login_data = {
-        'email': 'ivan.suhum@gmail.com',
-        'password': 'hPHprb2Z7JXdq8',
-        'remember_me': 1
-    }
 
-    r = requests.post('https://iqoption.com/api/login', login_data)
+    # ["{\"name\":\"ssid\",\"msg\":\"571c72efaf819381a5b445905eecda3c\"}"]
 
-
-    ws.send(json.dumps({"name":"ssid","msg":r.cookies['ssid']}))
+    ws.send(json.dumps({"name":"ssid","msg":"571c72efaf819381a5b445905eecda3c"}))
     ws.send(json.dumps({"name":"subscribe","msg":"deposited"}))
     ws.send(json.dumps({"name":"subscribe","msg":"tradersPulse"}))
     ws.send(json.dumps({"name":"subscribe","msg":"activeScheduleChange"}))
 
     # EURUSD-OTC
-    ws.send(json.dumps({"name":"setActives","msg":{"actives":[76]}}))
+    #ws.send(json.dumps({"name":"setActives","msg":{"actives":[76]}}))
+    # EURUSD
+    ws.send(json.dumps({"name":"setActives","msg":{"actives":[1]}}))
 
 
     ws.send(json.dumps({"name":"subscribe","msg":"activeCommissionChange"}))
     ws.send(json.dumps({"name":"unSubscribe","msg":"iqguard"}))
     ws.send(json.dumps({"name":"unSubscribe","msg":"signal"}))
+    ws.send(json.dumps({"name":"unSubscribe","msg":"timeSync"}))
     ws.send(json.dumps({"name":"unSubscribe","msg":"feedRecentBets"}))
     ws.send(json.dumps({"name":"unSubscribe","msg":"feedRecentBets2"}))
     ws.send(json.dumps({"name":"unSubscribe","msg":"feedTopTraders2"}))
@@ -78,11 +137,28 @@ def on_open(ws):
 
 
 if __name__ == "__main__":
-    websocket.enableTrace(True)
+    #websocket.enableTrace(True)
+
+    login_data = {
+        'email': 'ivan.suhum@gmail.com',
+        'password': 'hPHprb2Z7JXdq8',
+        'remember_me': 1
+    }
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    r = requests.post('https://iqoption.com/api/login', login_data, headers=headers)
+
+    logging.info("SSID: {}".format(r.cookies['ssid']))
+    ssid = r.cookies['ssid']
+
+
     ws = websocket.WebSocketApp("wss://eu.iqoption.com/echo/websocket",
                                 on_message=on_message,
                                 on_error=on_error,
                                 on_close=on_close)
+                                #cookie="skey=b10847bd8a14e4180fcd25ac35bc3239;ssid=571c72efaf819381a5b445905eecda3c")
     ws.on_open = on_open
     ws.run_forever()
 
